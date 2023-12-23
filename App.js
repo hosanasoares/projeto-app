43142af9c40f99668c4a69abae827eb0b1344a05
Tobias <tobiascosta064@gmail.com>
2023-12-23 01:17:34 -0300

    home_login_screen

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color:"#fff"}}>Welcome to the app</Text>
-      <Text style={{color:"#fff"}}>Vamos merendar!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./src/assets/logo.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.loginText}>Entre como:</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>PROFESSOR(A)</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>ALUNO(A)</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>NUTRICIONISTA</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.transparentButton}>
+        <Text style={styles.passwordText}>Esqueceu sua senha?</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,10 +32,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#db5353',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 354,
+    height: 330,
+    backgroundColor: 'lightgray', // ou a cor desejada
+    resizeMode: 'cover', // modo de redimensionamento
+  },
+  loginText: {
+    color: '#00000094',
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    marginBottom: 40,
+  },
+  button: {
+    width: 255,
+    height: 58,
+    borderRadius: 40,
+    backgroundColor: '#F22D1B',
+    elevation: 4, // Adiciona uma sombra
+    marginVertical: 10, // Espaçamento vertical entre os botões
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontFamily: 'Roboto',
+    fontSize: 25,
+    fontStyle: 'normal',
+    fontWeight: '700',
+  },
+  transparentButton: {
+    marginTop: 20,
+    width: 212,
+    height: 33,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  passwordText: {
+    color: '#F2A71B',
+    fontFamily: 'Roboto',
+    fontSize: 13,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    textDecorationLine: 'underline',
   },
 });
-
-
