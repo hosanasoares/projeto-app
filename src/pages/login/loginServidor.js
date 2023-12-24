fcd768d6dbdb4bf8e41a824753ad42cb041e19bc
Tobias <tobiascosta064@gmail.com>
2023-12-23 21:49:12 -0300

    update_login

--- src/pages/login/loginServidor.js
+++ src/pages/login/loginServidor.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 export function LoginServidor( { navigation } ) {
