acd1d4dd3b3fb6c83157edcbda854b68f4b6c835
hosanasoares <146008283+hosanasoares@users.noreply.github.com>
2024-02-02 09:02:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native' // envolve toda a estrutura de navegação da sua aplicação(navegação principal)
+import { createStackNavigator } from '@react-navigation/stack'; //  uma função que cria um navegador de pilha(varias paginas)
 
 import { LoginHome } from './src/pages/login/loginHome';
 import { LoginAluno } from './src/pages/login/loginAluno';
@@ -15,9 +15,9 @@ export default function App(){
       <Stack.Navigator initialRouteName="loginHome">
           <Stack.Screen 
             name="LoginHome"
-            component={LoginHome}
+            component={LoginHome} // algo deve ser importado e é responsável por toda a lógica e apresentação da tela de login 
             options={{
-               headerShown: false 
+               headerShown: false // está dizendo que não quer que o cabeçalho seja mostrado na tela ativa 
               }}
 
           />
